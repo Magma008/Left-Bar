01e19c63faabae7859c17ca7bb69d02440408896
Unknown <unknownmagma008@gmail.com>
2023-10-12 11:42:10 +0500

    12 October Thursday 2023 11:42 AM

---
+++ script.js
@@ -0,0 +1,14 @@
+const open = document.querySelector(".open")
+const close = document.querySelector(".close")
+const leftbar = document.querySelector(".left_bar")
+
+open.addEventListener("click", () => {
+    leftbar.classList.toggle("width")
+    open.classList.toggle("animateOpen")
+    close.classList.toggle("animateClose")
+})
+
+close.addEventListener("click", () => {
+    leftbar.classList.toggle("width")
+    open.classList.toggle("animateOpen")
+}) 
